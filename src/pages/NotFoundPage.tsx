74ae07ad4d0d10c0556e62b02bde026f9d275efa
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2025-09-08 20:13:23 +0200

    minor corrections:
    adjust `NotFoundPage` move away from justify-center to justify-start

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,7 +6,7 @@ import Button from "../components/ui/Button";
  */
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center px-4">
+    <div className="flex flex-col items-center justify-start h-screen text-center px-4">
       <h1 className="text-6xl md:text-9xl font-black text-neutral-50">404</h1>
       <h2 className="text-2xl md:text-4xl font-bold mt-4 text-neutral-200">Page Not Found</h2>
       <p className="text-neutral-200 text-lg mt-2 max-w-md">
